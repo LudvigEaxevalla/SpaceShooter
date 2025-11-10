69e2e41c0a019ae99b5cb8c6b91377efabfe4a5f
unknown <aspel25luei@axevalla.fhsk.se>
2025-11-10 14:09:56 +0100

    Damage and Canvas

--- Assets/Scripts/enemyScript.cs
+++ Assets/Scripts/enemyScript.cs
@@ -1,25 +1,34 @@
 using Unity.VisualScripting;
 using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class enemyScript : MonoBehaviour
 {
     public float enemySpeed = 3f;
+    public enemySpawner spawner;
+    public canvasScript Canvas;
+    public Text scoreText;
+    public int playerScore = 0;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        spawner = GameObject.Find("Enemy Spawner").GetComponent<enemySpawner>();
+        scoreText = GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        scoreText.text = playerScore.ToString();
         transform.Translate(Vector3.down * enemySpeed * Time.deltaTime);
 
         if (transform.position.y <= -5) {
             transform.position = new Vector3 (Random.Range(-8, 8), 5, transform.position.z);
         }
+   
 
     }
 
@@ -30,10 +39,16 @@ public class enemyScript : MonoBehaviour
         {
             other.transform.GetComponent<PlayerMovement>().TakeDamage();
             Debug.Log("Hit: " + other);
+            spawner.enemyCounter--;
+            Destroy(gameObject);
+
         }
 
         if (other.tag == "Bullet")
         {
+            Destroy(other.gameObject);
+            spawner.enemyCounter--;
+            playerScore++;
             Destroy(gameObject);
         }
     }
--- Assets/Scripts/enemySpawner.cs
+++ Assets/Scripts/enemySpawner.cs
@@ -5,7 +5,7 @@ public class enemySpawner : MonoBehaviour
 {
 
     public GameObject enemy;
-    private int enemyCounter;
+    public int enemyCounter;
     private GameObject player;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
